f6fca4178c91ea28e9324c50ca10b6e42f3d68f8
pgn4web <pgn4web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-14 23:47:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/embed-chessboard/trunk@263640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedchessboard.php
+++ embedchessboard.php
@@ -4,7 +4,7 @@
 Plugin Name: Embed Chessboard
 Plugin URI: http://wordpress.org/extend/plugins/embed-chessboard/
 Description: Embeds a javascript chessboard in wordpress articles for replaying chess games. Use plugin options to blend the chessboard with the site template; use tag parameters to customize each chessboard. Insert chess games in PGN format into your wordpress article using the syntax: <code>[pgn parameter=value ...] e4 e6 d4 d5 [/pgn]</code>. For more info on plugin options and tag parameters please <a href="http://code.google.com/p/pgn4web/wiki/User_Notes_wordpress">read the tutorial</a>.
-Version: 1.24
+Version: 1.25
 Author: Paolo Casaschi
 Author URI: http://pgn4web.casaschi.net
 
@@ -38,6 +38,7 @@ ChangeLog:
   1.22  - upgraded pgn4web to 2.02 with improved PGN error handling
   1.23  - upgraded pgn4web to 2.03
   1.24  - upgraded pgn4web to 2.04
+  1.25  - minor bug fix
 */
 
 class pgnBBCode {
@@ -57,7 +58,7 @@ class pgnBBCode {
 
 		// [pgn height=600 showMoves=justified initialGame=1 initialHalfmove=0 autoplayMode=loop] e4 e6 d4 d5 [/pgn]
 
-		$pgnText = preg_replace("@<.*?>@", "", $content);
+		$pgnText = preg_replace("@(<.*?>|\n)@", " ", $content);
 
 		if ( isset($atts['layout']) ) { $layout = $atts['layout']; }
 		elseif ( isset($atts['l']) ) { $layout = $atts['l']; }
