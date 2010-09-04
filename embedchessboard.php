21d47760f13e2dc53ab335d4f0596015c2cb28b6
pgn4web <pgn4web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-04 19:54:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/embed-chessboard/trunk@285125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embedchessboard.php
+++ embedchessboard.php
@@ -4,7 +4,7 @@
 Plugin Name: Embed Chessboard
 Plugin URI: http://wordpress.org/extend/plugins/embed-chessboard/
 Description: Embeds a javascript chessboard in wordpress articles for replaying chess games. Use plugin options to blend the chessboard with the site template; use tag parameters to customize each chessboard. Insert chess games in PGN format into your wordpress article using the syntax: <code>[pgn parameter=value ...] e4 e6 d4 d5 [/pgn]</code>. For more info on plugin options and tag parameters please <a href="http://code.google.com/p/pgn4web/wiki/User_Notes_wordpress">read the tutorial</a>.
-Version: 1.27
+Version: 1.28
 Author: Paolo Casaschi
 Author URI: http://pgn4web.casaschi.net
 
@@ -41,6 +41,7 @@ ChangeLog:
   1.25  - minor bug fix
   1.26  - added rawurlencode() to url parameters and upgraded pgn4web to 2.05
   1.27  - added extendedOptions switch to the [pgn] tag and upgraded pgn4web to 2.06
+  1.28  - upgraded pgn4web to 2.07, inlcuding Chess960 support
 */
 
 class pgnBBCode {
--- pgn4web/README.txt
+++ pgn4web/README.txt
@@ -335,6 +335,10 @@ and stores the internal comment value for internal use.
 
 Please email me for review any PGN file that pgn4web fails parsing correctly. 
 
+CHESS960 SUPPORT
+
+pgn4web supports Chess960 (a.k.a. Fischer random chess) and understands both 
+the X-FEN and the Shredder-FEN extensions to the FEN notation.
 
 JAVASCRIPT CODING
 
